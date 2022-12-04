e7c80c21880369810f50e3730292c9443de5d34c
Johan Herland <johan@herland.net>
2022-12-04 02:46:20 +0100

    More golfing on day02

--- day02.rs
+++ day02.rs
@@ -3,116 +3,111 @@ use std::str::FromStr;
 
 use anyhow::{anyhow, Error, Result};
 
-#[derive(Debug, Clone, Copy)]
-enum RPS {
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum Move {
     Rock,
     Paper,
     Scissors,
 }
 
-impl RPS {
-    fn fight(&self, opponent: &RPS) -> Outcome {
-        match (self, opponent) {
-            (RPS::Rock, RPS::Rock) | (RPS::Paper, RPS::Paper) | (RPS::Scissors, RPS::Scissors) => {
-                Outcome::Draw
-            }
-            (RPS::Rock, RPS::Paper) | (RPS::Paper, RPS::Scissors) | (RPS::Scissors, RPS::Rock) => {
-                Outcome::Loss
-            }
-            (RPS::Rock, RPS::Scissors) | (RPS::Paper, RPS::Rock) | (RPS::Scissors, RPS::Paper) => {
-                Outcome::Win
-            }
-        }
-    }
+use Move::*;
 
-    fn score(&self, opponent: &RPS) -> u32 {
-        let mychoice = match self {
-            RPS::Rock => 1,
-            RPS::Paper => 2,
-            RPS::Scissors => 3,
-        };
-        let outcome = match self.fight(opponent) {
-            Outcome::Loss => 0,
-            Outcome::Draw => 3,
-            Outcome::Win => 6,
-        };
-        mychoice + outcome
-    }
-
-    fn choose(&self, outcome: &Outcome) -> RPS {
-        match &outcome {
-            Outcome::Win => match &self {
-                RPS::Rock => RPS::Paper,
-                RPS::Paper => RPS::Scissors,
-                RPS::Scissors => RPS::Rock,
-            },
-            Outcome::Draw => *self,
-            Outcome::Loss => match &self {
-                RPS::Rock => RPS::Scissors,
-                RPS::Paper => RPS::Rock,
-                RPS::Scissors => RPS::Paper,
-            },
-        }
-    }
-}
-
-impl FromStr for RPS {
+impl FromStr for Move {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "A" | "X" => Ok(RPS::Rock),
-            "B" | "Y" => Ok(RPS::Paper),
-            "C" | "Z" => Ok(RPS::Scissors),
+            "A" | "X" => Ok(Rock),
+            "B" | "Y" => Ok(Paper),
+            "C" | "Z" => Ok(Scissors),
             _ => Err(anyhow!("Failed to parse RPS from {s:?}")),
         }
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 enum Outcome {
     Win,
     Draw,
     Loss,
 }
 
+use Outcome::*;
+
 impl FromStr for Outcome {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "X" => Ok(Outcome::Loss),
-            "Y" => Ok(Outcome::Draw),
-            "Z" => Ok(Outcome::Win),
+            "X" => Ok(Loss),
+            "Y" => Ok(Draw),
+            "Z" => Ok(Win),
             _ => Err(anyhow!("Failed to parse Outcome from {s:?}")),
         }
     }
 }
 
+struct Fight(Move, Move, Outcome);
+
+const VALID_FIGHTS: [Fight; 9] = [
+    // (opponent move, my move, outcome)
+    Fight(Rock, Paper, Win),
+    Fight(Paper, Scissors, Win),
+    Fight(Scissors, Rock, Win),
+    Fight(Rock, Rock, Draw),
+    Fight(Paper, Paper, Draw),
+    Fight(Scissors, Scissors, Draw),
+    Fight(Rock, Scissors, Loss),
+    Fight(Paper, Rock, Loss),
+    Fight(Scissors, Paper, Loss),
+];
+
+impl Fight {
+    fn parse_move_vs_move(line: &str) -> &'static Fight {
+        let (a, b) = line.split_once(' ').unwrap();
+        VALID_FIGHTS
+            .iter()
+            .find(|f| f.0 == a.parse().unwrap() && f.1 == b.parse().unwrap())
+            .unwrap()
+    }
+
+    fn parse_move_and_outcome(line: &str) -> &'static Fight {
+        let (a, c) = line.split_once(' ').unwrap();
+        VALID_FIGHTS
+            .iter()
+            .find(|f| f.0 == a.parse().unwrap() && f.2 == c.parse().unwrap())
+            .unwrap()
+    }
+
+    fn score(&self) -> u32 {
+        (match self.1 {
+            Rock => 1,
+            Paper => 2,
+            Scissors => 3,
+        }) + (match self.2 {
+            Loss => 0,
+            Draw => 3,
+            Win => 6,
+        })
+    }
+}
+
 fn main() {
     let lines: Vec<_> = io::stdin().lines().map(Result::unwrap).collect();
-    let char_pairs: Vec<_> = lines
-        .iter()
-        .map(|line| line.split_once(" ").unwrap())
-        .collect();
 
     println!(
         "Part 1: {}",
-        char_pairs
+        lines
             .iter()
-            .map(|(a, b)| b.parse::<RPS>().unwrap().score(&a.parse().unwrap()))
+            .map(|line| Fight::parse_move_vs_move(line).score())
             .sum::<u32>()
     );
 
     println!(
         "Part 2: {}",
-        char_pairs
+        lines
             .iter()
-            .map(|(a, b)| {
-                let opponent: RPS = a.parse().unwrap();
-                let outcome: Outcome = b.parse().unwrap();
-                opponent.choose(&outcome).score(&opponent)
-            })
+            .map(|line| Fight::parse_move_and_outcome(line).score())
             .sum::<u32>()
     );
 }
