1207e05aceff6fe82e5ce999929f6b3e24602b30
Johan Herland <johan@herland.net>
2022-12-06 10:15:57 +0100

    06: Finally something straightforward and nice :-)

---
+++ day06.rs
@@ -0,0 +1,27 @@
+use std::collections::HashSet;
+use std::io;
+
+fn find_first_pos_after_n_unique(s: &str, n: usize) -> Option<usize> {
+    s.as_bytes()
+        .windows(n)
+        .enumerate()
+        .map(|(i, win)| (i + n, win.iter().copied().collect::<HashSet<u8>>().len()))
+        .find(|(_, unique)| *unique == n)
+        .map(|(pos, _)| pos)
+}
+
+pub fn main() {
+    let mut line = String::new();
+    io::stdin()
+        .read_line(&mut line)
+        .expect("Failed to read line");
+
+    println!(
+        "Part 1: {}",
+        find_first_pos_after_n_unique(&line, 4).unwrap()
+    );
+    println!(
+        "Part 2: {}",
+        find_first_pos_after_n_unique(&line, 14).unwrap()
+    );
+}
